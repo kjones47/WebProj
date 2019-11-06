708701df608f50474de668e93deb15652b3c8702
Kenisha Jones <kjones47@binghamton.edu>
2019-11-05 21:14:45 -0500

    pixel

---
+++ Proj5/app/src/main/java/com/example/android/proj5/Pixel.java
@@ -0,0 +1,22 @@
+package com.example.android.proj5;
+
+public class Pixel {
+    int r;
+    int g;
+    int b;
+    int num;
+
+    public Pixel(){
+
+    }
+
+    public int getRVal(){
+        return r;
+    }
+    public int getG(){
+        return g;
+    }
+    public int getB(){
+        return b;
+    }
+}
